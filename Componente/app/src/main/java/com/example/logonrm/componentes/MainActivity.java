38a3ced3f233db2281e67e94b3e5bcfd6fa2f376
logonrm <logonrm@fiap.com.br>
2018-03-01 18:48:48 -0300

    Criado projetos android| troca de imagens | jogo dados

---
+++ Componente/app/src/main/java/com/example/logonrm/componentes/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.logonrm.componentes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Boolean imagemAlterada = false;
+    ImageView imgPessoa;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imgPessoa = findViewById(R.id.imgPessoa);
+    }
+
+    public void alterarImagem(View view) {
+        if(! imagemAlterada) {
+            imgPessoa.setImageResource(R.drawable.person2);
+            imagemAlterada = true;
+        }else{
+            imgPessoa.setImageResource((R.drawable.person1));
+            imagemAlterada = false;
+        }
+    }
+}
